84ee8f2650d6423bb361838f6b4ce2058112b560
Krzysztof Słonka <slonka@student.agh.edu.pl>
2012-12-07 18:07:08 +0100

    ExampleData usage

--- inserts.sql
+++ inserts.sql
@@ -1,3 +1,20 @@
+------------------------------------------------
+
+/*
+ * Uzywanie danych z bazy ExampleData:
+ * Tworzymy sobie zapytanie
+ * select 'insert into tabela ([kolumna1], [kolumna2], ...) values (''' + wartosc_typu_varchar +''', '+ wartosc_int +', ...); from GeneralData '
+ * wybierajac w nim dane jakie nam sa potrzebne
+ * to tworzy nam polecenia które wystarczy przekopiowaæ do edytora i wykonaæ (F5)
+ * trzeba tylko pamiêtaæ aby zaznaczyæ "Results to text" na pasku
+ * przyklad
+*/
+
+use ExampleData
+select 'insert into tabela ([id], [City], [ZipCode], [Street], [Phone]) values (' + cast(id as varchar(10)) +', '''+ City+''', '''+ ZipCode+''', '''+ Street+''', '''+ Phone+''');' from GeneralData 
+
+------------------------------------------------
+
 use test1
 
 -- venue
